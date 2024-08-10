8d945cb9906bb0e04536ee73a2c489ecacb9a599
shweta <shweta.saxena@appinventiv.com>
2024-08-10 16:26:10 +0530

    GET api implement

---
+++ getData.js
@@ -0,0 +1,19 @@
+const data = {
+  data: [
+    {
+      title: "Node js",
+      description: "its a GET api",
+      subData: {
+        id: "1234",
+        route: "./index.js",
+        checkStatus: "active",
+      },
+    },
+    {
+      title: "Node js",
+      description: "Checking api data",
+    },
+  ],
+};
+
+module.exports = data;
--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const http = require("http");
+const data = require("./getData");
 
 http
   .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(
-      JSON.stringify({
-        data: "Hello World",
-      })
-    );
+    res.writeHead(200, { "Content-Type": "application/json" ,"access-control-allow-headers": "*"});
+    res.write(JSON.stringify(data))
+      res.end();
+
   })
   .listen(8000);
